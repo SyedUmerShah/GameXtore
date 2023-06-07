9fe80cef3e8bfa7bb405850d2523e2a6700626e0
Syed Umer Shah <101082333+SyedUmerShah@users.noreply.github.com>
2023-06-07 15:40:51 +0500

    Add files via upload

---
+++ GameXtore/app.js
@@ -0,0 +1,331 @@
+var products = {
+    fantechMouse: {
+        mouse1: {
+            name: "DAREDEVIL X11",
+            price: "Rs.2000",
+            availabity: "In Stock",
+            img: "img/X11_black_icon_758x455.png",
+        },
+        mouse2: {
+            name: "BLAKE X17 SAKURA",
+            price: "Rs.2,750",
+            availabity: "In Stock",
+            img: "img/X17_pink_icon_758x455-500x300.png",
+        },
+        mouse3: {
+            name: "RANGERS X14s",
+            price: "Rs.5,700",
+            availabity: " In Stock",
+            img: "img/X14s_black_icon_758x455-500x300.png",
+        },
+        mouse4: {
+            name: "TITAN X4s",
+            price: "Rs.12,000",
+            availabity: "In Stock",
+            img: "img/X4s_black_icon_758x455-500x300.png",
+        }
+        ,
+        mouse5: {
+            name: "CRYPTO VX7",
+            price: "Rs.6,000",
+            availabity: "In Stock",
+            img: "img/VX7_black_icon_758x455-500x300.png",
+        }
+    },
+    fantechKeyboard: {
+        keyboard1: {
+            name: "ATOM MK886",
+            price: "Rs.2,900",
+            availabity: "In Stock",
+            img: "img/MK886_black_icon_758x455px.png",
+        },
+        keyboard2: {
+            name: "MAXFIT61 FROST",
+            price: "Rs.3,750",
+            availabity: "Out of Stock",
+            img: "img/Maxfit61-Frost_white_icon_758x455px.png",
+        },
+        keyboard3: {
+            name: "MAXPOWER SPACE",
+            price: "Rs.4,350",
+            availabity: "In Stock",
+            img: "img/MK853_white_icon_758x455px-500x300.png",
+        },
+        keyboard4: {
+            name: "MAX CORE MK852",
+            price: "Rs.5,700",
+            availabity: "In Stock",
+            img: "img/MK852_black_icon_758x455px-500x300.png",
+        },
+        keyboard5: {
+            name: "FIGHTER TKL II K613L",
+            price: "Rs.5,900",
+            availabity: "Out of Stock",
+            img: "img/K613L_black_icon_758x455px.png",
+        }
+    },
+    fantechHeadset: {
+        headset1: {
+            name: "ALTO 7.1 HG26",
+            price: "Rs.5,200",
+            availabity: "In Stock",
+            img: "img/ALTO-HG26.png",
+        },
+        headset2: {
+            name: "TAMAGO",
+            price: "Rs.4,490",
+            availabity: "In Stock",
+            img: "img/Fantech-Tamago.png",
+        },
+        headset3: {
+            name: "WH01 MINT EDITION",
+            price: "Rs.4,700",
+            availabity: "Out of Stock",
+            img: "img/WH01-1000X600.png",
+        },
+        headset4: {
+            name: "SONATA MH90",
+            price: "Rs.12,900",
+            availabity: "In Stock",
+            img: "img/HG11SA-1-500x300.png",
+        },
+        headset5: {
+            name: "MITHRIL TX1",
+            price: "Rs.6,500",
+            availabity: "In Stock",
+            img: "img/TX1-4-min.png",
+        }
+    },
+    bloodyMouse: {
+        mouse1: {
+            name: "R90 Plus",
+            price: "Rs.3000",
+            availabity: "In Stock",
+            img: "img/20221205110748_27149.png",
+        },
+        mouse2: {
+            name: "W60 Pro",
+            price: "Rs.3,750",
+            availabity: "In Stock",
+            img: "img/20220421092315_14619.png",
+        },
+        mouse3: {
+            name: "W95 Max Sports",
+            price: "Rs.4,500",
+            availabity: " In Stock",
+            img: "img/20230518143727_82575.png",
+        },
+        mouse4: {
+            name: "A70",
+            price: "Rs.6,000",
+            availabity: "In Stock",
+            img: "img/20220421094301_75843.png",
+        }
+        ,
+        mouse5: {
+            name: "Q50",
+            price: "Rs.4,000",
+            availabity: "In Stock",
+            img: "img/20220421102636_86429.png",
+        }
+    },
+    bloodyKeyboard: {
+        keyboard1: {
+            name: "S98 Naraka",
+            price: "Rs.8,900",
+            availabity: "In Stock",
+            img: "img/20230224164328_79425.png",
+        },
+        keyboard2: {
+            name: "S98 Sports",
+            price: "Rs.9,750",
+            availabity: "Out of Stock",
+            img: "img/20230223101021_51509.png",
+        },
+        keyboard3: {
+            name: "S98 Aviator",
+            price: "Rs.10,350",
+            availabity: "In Stock",
+            img: "img/20230223095424_99711.png",
+        },
+        keyboard4: {
+            name: "S510",
+            price: "Rs.8,700",
+            availabity: "In Stock",
+            img: "img/20220610112346_88730.png",
+        },
+        keyboard5: {
+            name: "S510N",
+            price: "Rs.11,900",
+            availabity: "Out of Stock",
+            img: "img/20220818134519_14985.png",
+        }
+    },
+    bloodyHeadset: {
+        headset1: {
+            name: "M320",
+            price: "Rs.9,200",
+            availabity: "In Stock",
+            img: "img/20211228171424_69740.png",
+        },
+        headset2: {
+            name: "MR575",
+            price: "Rs.8,490",
+            availabity: "In Stock",
+            img: "img/20230201160325_84989.png",
+        },
+        headset3: {
+            name: "MR710",
+            price: "Rs.10,700",
+            availabity: "Out of Stock",
+            img: "img/20220118140118_86324.png",
+        },
+        headset4: {
+            name: "MR720",
+            price: "Rs.12,900",
+            availabity: "In Stock",
+            img: "img/20230220102227_73668.png",
+        },
+        headset5: {
+            name: "MH390",
+            price: "Rs.9,500",
+            availabity: "In Stock",
+            img: "img/20230424145903_71460.png",
+        }
+    },
+    razerMouse: {
+        mouse1: {
+            name: "RAZER HYPERSPEED",
+            price: "Rs.3000",
+            availabity: "In Stock",
+            img: "img/https___hybrismediaprod.blob.core.windows.net_sys-master-phoenix-images-container_h3a_hb0_9529652609054_basilisk-v3-x-hyperspeed-2-500x500.png"
+        },
+        mouse2: {
+            name: "BASILISK V3 PRO",
+            price: "Rs.3,750",
+            availabity: "In Stock",
+            img: "img/https___hybrismediaprod.blob.core.windows.net_sys-master-phoenix-images-container_h5f_hba_9529652314142_basilisk-v3-pro-white-2-500x500.png",
+        },
+        mouse3: {
+            name: "RAZER V3 PRO",
+            price: "Rs.4,500",
+            availabity: " In Stock",
+            img: "img/https___hybrismediaprod.blob.core.windows.net_sys-master-phoenix-images-container_ha0_hb6_9529652445214_basilisk-v3-pro-black-2-500x500.png",
+        },
+        mouse4: {
+            name: "RAZER ULTIMATE",
+            price: "Rs.6,000",
+            availabity: "In Stock",
+            img: "img/https___hybrismediaprod.blob.core.windows.net_sys-master-phoenix-images-container_h11_hac_9529652772894_basilisk-ultimate-charging-dock-2-500x500.png",
+        }
+        ,
+        mouse5: {
+            name: "Razer V2",
+            price: "Rs.4,000",
+            availabity: "In Stock",
+            img: "img/https___hybrismediaprod.blob.core.windows.net_sys-master-phoenix-images-container_hb7_h71_9080617041950_Basilisk-V2-500x500.png",
+        }
+    },
+    razerKeyboard: {
+        keyboard1: {
+            name: "RAZER BLACKWIDOW",
+            price: "Rs.13,900",
+            availabity: "In Stock",
+            img: "img/https___hybrismediaprod.blob.core.windows.net_sys-master-phoenix-images-container_hce_h3e_9512023195678_blackwidow-v3-roblox-2-500x500.png",
+        },
+        keyboard2: {
+            name: "RAZER V4 PRO",
+            price: "Rs.10,750",
+            availabity: "Out of Stock",
+            img: "img/https___hybrismediaprod.blob.core.windows.net_sys-master-phoenix-images-container_hcf_h0e_9479906000926_bw-v4-pro-500x500.png",
+        },
+        keyboard3: {
+            name: "RAZER V3 PRO",
+            price: "Rs.12,350",
+            availabity: "In Stock",
+            img: "img/https___hybrismediaprod.blob.core.windows.net_sys-master-phoenix-images-container_h6b_h53_9420855967774_bape-blackwidow-v3-500x500.png",
+        },
+        keyboard4: {
+            name: "BLACKWIDOW V3",
+            price: "Rs.14,700",
+            availabity: "In Stock",
+            img: "img/https___hybrismediaprod.blob.core.windows.net_sys-master-phoenix-images-container_h78_he7_9245362159646_blackwidow-v3-halo-infinite-500x500.png",
+        },
+        keyboard5: {
+            name: "MINI HYPERSPEED",
+            price: "Rs.9,900",
+            availabity: "Out of Stock",
+            img: "img/https___hybrismediaprod.blob.core.windows.net_sys-master-phoenix-images-container_h9d_hc8_9207479861278_razer-blackwidow-v3-mini-hyperspeed-phantom_500x500.png",
+        }
+    },
+    razerHeadset: {
+        headset1: {
+            name: "BLACKSHARK V2",
+            price: "Rs.13,200",
+            availabity: "In Stock",
+            img: "img/https___hybrismediaprod.blob.core.windows.net_sys-master-phoenix-images-container_hd5_hd4_9510180978718_blackshark-v2-pro-black-2023-500x500.png",
+        },
+        headset2: {
+            name: "RAZER PRO",
+            price: "Rs.15,490",
+            availabity: "In Stock",
+            img: "img/https___hybrismediaprod.blob.core.windows.net_sys-master-phoenix-images-container_h6d_he0_9510181044254_blackshark-v2-pro-white-2023-500x500.png",
+        },
+        headset3: {
+            name: "BLACKSHARK V2 X USB",
+            price: "Rs.18,700",
+            availabity: "Out of Stock",
+            img: "img/https___hybrismediaprod.blob.core.windows.net_sys-master-phoenix-images-container_h9c_h79_9431346348062_blackshark-v2-x-usb-500x500.png",
+        },
+        headset4: {
+            name: "ESL Edition",
+            price: "Rs.11,900",
+            availabity: "In Stock",
+            img: "img/https___hybrismediaprod.blob.core.windows.net_sys-master-phoenix-images-container_h80_hf9_9522060525598_blackshark-v2-esl-500x500.png",
+        },
+        headset5: {
+            name: "V2 X - Green",
+            price: "Rs.20,500",
+            availabity: "In Stock",
+            img: "img/https___hybrismediaprod.blob.core.windows.net_sys-master-phoenix-images-container_h8b_h62_9242259816478_blackshark-v2-x-green-500x500.png",
+        }
+    }
+};
+var content = document.getElementById("content");
+function allItems() {
+    content.innerHTML = ""
+    for (var one in products) {
+        for (var two in products[one]) {
+            content.innerHTML += `
+  <div class="col-12 col-sm-12 col-md-6 col-lg-4">
+  <div class="card">
+<img class="card-img" src="${products[one][two].img}" alt="">
+  <div class="card-body">
+  <p class="card-name">${products[one][two].name}.</p>
+  <h5 class="card-price"> ${products[one][two].price} </h5>
+  <p class="card-text">${products[one][two].availabity}.</p>
+  <button> Add To Cart</button>
+  </div>
+</div>
+  </div>
+ `}
+    }
+}
+allItems()
+function selectItem(brand) {
+    content.innerHTML = ""
+    for (var key in products[brand]) {
+        content.innerHTML += `
+        <div class="col-12 col-sm-12 col-md-6 col-lg-4">
+        <div class="card">
+      <img class="card-img" src="${products[brand][key].img}" alt="">
+        <div class="card-body">
+        <p class="card-name">${products[brand][key].name}.</p>
+        <h5 class="card-price"> ${products[brand][key].price} </h5>
+        <p class="card-text">${products[brand][key].availabity}.</p>
+        <button> Add To Cart</button>
+        </div>
+      </div>
+        </div>`
+    }
+}
